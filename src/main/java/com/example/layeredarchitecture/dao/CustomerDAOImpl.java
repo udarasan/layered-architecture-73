b0d08739a3c48bc789f7b3d54c2c1c7472faf132
Udara San <udarassanjeewa@gmail.com>
2025-07-02 11:03:06 +0530

    Initial commit

---
+++ src/main/java/com/example/layeredarchitecture/dao/CustomerDAOImpl.java
@@ -0,0 +1,27 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.CustomerDTO;
+import com.example.layeredarchitecture.view.tdm.CustomerTM;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class CustomerDAOImpl {
+    public ArrayList<CustomerDTO> getAllCustomer() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+        ArrayList<CustomerDTO> customers = new ArrayList<>();
+        while (rst.next()) {
+            String id = rst.getString("id");
+            String name = rst.getString("name");
+            String address = rst.getString("address");
+            customers.add(new CustomerDTO(id, name, address));
+        }
+        return customers;
+    }
+}
